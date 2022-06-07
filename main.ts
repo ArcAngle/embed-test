80360d91181749babeab723883d7155c1245f2aa
ArcAngle <eddy@3os.mobi>
2022-06-07 09:57:14 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Meh)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Tortoise)
+})
+basic.showIcon(IconNames.Heart)
